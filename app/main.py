534f49ebd9a21fee0e64a6b865d685989e8734f0
Matheus Hirota <hirota.dev@gmail.com>
2023-08-20 07:24:58 -0300

    Subindo metodo de geração via youtube completo

--- app/main.py
+++ app/main.py
@@ -1,10 +1,33 @@
 import tempfile
+import openai
+import mysql.connector
 from fastapi import FastAPI
+from langchain.chat_models import ChatOpenAI
+import requests
 from app.modules.stream.youtube import download_audio_from_youtube
 from app.modules.stream.split import split_audio_into_chunks
 from app.modules.transcribe.whisper import transcribe_audio
 from app.modules.summarize.langchain import summarize_langchain
+
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SimpleSequentialChain
+
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from PIL import Image as PILImage
+from io import BytesIO
+
 app = FastAPI()
+
+# connection = mysql.connector.connect(
+#   host="http://database-bard-api.ctofnlryccp5.us-east-1.rds.amazonaws.com",
+#   user="admin",
+#   password="ieTk^2aT2X2hA!1IQo$PqA",
+#   database="teste"
+# )
 
 @app.get('/summarize/youtube')
 def youtube(uri: str, category: str, grade: str):
@@ -26,6 +49,47 @@ def youtube(uri: str, category: str, grade: str):
             transcriptions.append(transcription)
     merged_transcription = '\n'.join([item['text'] for item in transcriptions])
     summarization = summarize_langchain(merged_transcription, category, grade, 'no')
-    return {
-        "summarization": summarization
-    }
+    
+    llm = ChatOpenAI(temperature=.7)
+    template = """Você irá receber um texto e deverá extrair o contexto dele, com base nesse contexto você deverá gerar 3 descrições de imagens para esse contexto
+    Texto: {text}
+    Descrições:
+    """
+    prompt_template = PromptTemplate(input_variables=["text"], template=template)
+    answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+    answer = answer_chain.run(summarization)
+    prompts = answer.split('\n')
+    images = []
+    for prompt in prompts:
+        if len(prompt) > 1 :
+            response = openai.Image.create(
+                prompt=prompt,
+                n=1,
+                size="512x512",
+            )
+            image_data = response["data"][0]["url"]
+            images.append(image_data)
+
+    output_pdf = "output.pdf"
+    doc = SimpleDocTemplate(output_pdf, pagesize=letter)
+    story = []
+
+    paragraphs = summarization.split('\n\n')
+
+    for idx, paragraph in enumerate(paragraphs):
+        styles = getSampleStyleSheet()
+        story.append(Paragraph(paragraph, styles['Normal']))
+        story.append(Spacer(1, 15))
+        if idx < 3:
+            image_data = BytesIO(requests.get(images[idx]).content)
+            img = PILImage.open(image_data)
+            width, height = img.size
+            aspect = height / float(width)
+            img_width = 512
+            img_height = img_width * aspect
+            img = Image(image_data, width=img_width, height=img_height)
+            story.append(img)
+            story.append(Spacer(1, 5))
+    
+    doc.build(story)
+    print(f"PDF criado com sucesso: {output_pdf}")
